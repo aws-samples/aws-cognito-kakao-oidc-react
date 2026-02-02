6c25863ee6e0406a5e9fc4c1a27b97cd0a73f125
Chulwoo Choi <prorhap@gmail.com>
2026-02-02 19:56:10 +0900

    Reset Repo

--- cdk/lib/cognito-kakao.d.ts
+++
@@ -1,7 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { aws_cognito as cognito } from 'aws-cdk-lib';
-export declare class CognitoKakao extends Construct {
-    readonly userPool: cognito.UserPool;
-    constructor(scope: Construct, id: string, props?: cdk.StackProps);
-}
